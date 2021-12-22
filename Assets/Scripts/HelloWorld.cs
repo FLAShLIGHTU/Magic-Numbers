2f2e801aac601d85a4dec1b0d1a469bafd702c7b
Ivan Panchykau <xenoonz1unity@gmail.com>
2021-12-23 00:38:21 +0300

    Add ChekWin

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -3,41 +3,69 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class HelloWorld : MonoBehaviour
 {
- [SerializeField] private int min;
- [SerializeField] private int max;
- private int guess;
-
+ [SerializeField] private int _min;
+ [SerializeField] private int _max;
+ private int _guess;
+ private int _counterSteps;
+ 
  private void Start()
  {
-  Debug.Log($"Загадай число от {min} до {max}");
-  
-    
+  Debug.Log($"Загадай число от {_min} до {_max} и нажми любую кнопку");
+  _counterSteps = 0;
+
  }
 
  private void Update()
  {
   if (Input.GetKeyDown(KeyCode.DownArrow))
   {  
-   max = guess;
-   this.CalculateGuess();
+   _max = _guess;
+   
+   CalculateGuess();
+   CheckWin();
   }
   if (Input.GetKeyDown(KeyCode.UpArrow))
   {
-   min = guess;
-   this.CalculateGuess();
+   _min = _guess;
+   
+   CalculateGuess();
+   CheckWin();
+   
   }
   if (Input.GetKeyDown(KeyCode.Space))
   {
    
   }
-
+  
+  
  }
  private void CalculateGuess()
  {
-  guess = (min + max) / 2;
-  Debug.Log($"Твое число {guess} ?");
+  _guess = (_min + _max) / 2;
+  Debug.Log($"Твое число {_guess} ?");
+  _counterSteps += 1;
+
+ }
+
+ private void CheckWin()
+ 
+ {
+  Debug.Log("Это твое число? Y - да, N - нет.");
+  
+  if (Input.GetKeyDown(KeyCode.Y))
+  {
+   Debug.Log($"Ура! Колличество шагов {_counterSteps}");
+   
+  }
+
+  if (Input.GetKeyDown(KeyCode.N))
+  {
+   Update();
+  }
+
  }
 }
