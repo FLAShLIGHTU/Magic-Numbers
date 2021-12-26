75a4c8629f3c0a93ccf3cf6af4909b29eb2631cd
Ivan Panchykau <xenoonz1unity@gmail.com>
2021-12-26 17:19:57 +0300

    Deleted old scripts

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,7 +12,7 @@ public class UIController : MonoBehaviour
     private int _sumNumber;
     private int _number;
     private int _counterSteps;
-    // Start is called before the first frame update
+
     void Start()
     {
         _sumNumber = 0;
@@ -18,119 +20,126 @@ public class UIController : MonoBehaviour
         _counterSteps = 0;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-                if (Input.GetKeyDown(KeyCode.Space))
-        {
-            _sumNumber = 0;
-            _counterSteps = 0;
-        }
         
+
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             _number = 1;
             _sumNumber = _sumNumber + _number;
             myText.text = ($"Вы ввели {_number}. Ваша сумма {_sumNumber}");
             winCheker();
-            
+
 
         }
-        
+
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             _number = 2;
             _sumNumber = _sumNumber + _number;
             myText.text = ($"Вы ввели {_number}. Ваша сумма {_sumNumber}");
             winCheker();
-            
+
 
         }
-        
+
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             _number = 3;
             _sumNumber = _sumNumber + _number;
             myText.text = ($"Вы ввели {_number}. Ваша сумма {_sumNumber}");
             winCheker();
-            
+
 
         }
-        
+
         if (Input.GetKeyDown(KeyCode.Alpha4))
         {
             _number = 4;
             _sumNumber = _sumNumber + _number;
             myText.text = ($"Вы ввели {_number}. Ваша сумма {_sumNumber}");
             winCheker();
-            
+
 
         }
-        
+
         if (Input.GetKeyDown(KeyCode.Alpha5))
         {
             _number = 5;
             _sumNumber = _sumNumber + _number;
             myText.text = ($"Вы ввели {_number}. Ваша сумма {_sumNumber}");
             winCheker();
-            
+
 
         }
-        
+
         if (Input.GetKeyDown(KeyCode.Alpha6))
         {
             _number = 6;
             _sumNumber = _sumNumber + _number;
             myText.text = ($"Вы ввели {_number}. Ваша сумма {_sumNumber}");
             winCheker();
-            
+
 
         }
-        
+
         if (Input.GetKeyDown(KeyCode.Alpha7))
         {
             _number = 7;
             _sumNumber = _sumNumber + _number;
             myText.text = ($"Вы ввели {_number}. Ваша сумма {_sumNumber}");
             winCheker();
-            
+
 
         }
-        
+
         if (Input.GetKeyDown(KeyCode.Alpha8))
         {
             _number = 8;
             _sumNumber = _sumNumber + _number;
             myText.text = ($"Вы ввели {_number}. Ваша сумма {_sumNumber}");
             winCheker();
-            
+
 
         }
-        
+
         if (Input.GetKeyDown(KeyCode.Alpha9))
         {
             _number = 9;
             _sumNumber = _sumNumber + _number;
             myText.text = ($"Вы ввели {_number}. Ваша сумма {_sumNumber}");
             winCheker();
-            
 
+
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _sumNumber = 0;
+            _counterSteps = 0;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            enabled = false;
         }
 
     }
 
-    private bool _boolSumNumber(int _sumNumber)
-    {
-        return (_sumNumber == 1);
-    }
     private void winCheker()
     {
         _counterSteps += 1;
-        if(_sumNumber >= _lastNumber)
+        if (_sumNumber >= _lastNumber)
         {
             myText.text = ($"Игра окончена! Колличество ходов:{_counterSteps} Нажмите пробел, что бы начать заново.");
+            
 
-           
+
+
+
         }
     }
-    }
+}
+   
